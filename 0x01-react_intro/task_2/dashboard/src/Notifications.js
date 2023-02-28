df127e10e4d1b993804452698f222144bac49055
phiona-b3 <phiona030303@gmail.com>
2023-02-28 22:05:34 +0300

    Task

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -6,19 +6,18 @@ import { getLatestNotification } from "./utils";
 function Notifications() {
   return (
     <div className="Notifications">
-      <button 
-        style={{ color:"#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontsize: "15px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
-        aria-label="close"
+      <button
+        style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "15px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
+        aria-label="Close"
         onClick={console.log("Close button has been clicked")}
       >
-        <img src={closeIcon} alt="closeIcon" />
-
+        <img src={closeIcon} alt="closeIcon" width="10px" />
       </button>
       <p>Here is the list of notifications</p>
       <ul>
         <li data="default">New course available</li>
         <li data="urgent">New resume available</li>
-        <li data="urgent " dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+        <li data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
       </ul>
     </div>
   );
